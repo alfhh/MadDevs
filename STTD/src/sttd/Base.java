cef8f2a5b53ef681d28417ddbeff04d57032974a
Alfredo Hinojosa <alfhh4@gmail.com>
2014-04-07 14:30:19 -0500

    Header

---
+++ STTD/src/sttd/Base.java
@@ -0,0 +1,120 @@
+/**
+ * @author Alfredo Hinojosa Huerta A01036053
+ * @author Luis Juan Sanchez A01183634
+ * @version 1.00 03/11/2014
+ */
+
+package sttd;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import java.awt.Rectangle;
+
+public class Base {
+    
+    private int posX;    //posicion en x.       
+    private int posY;	//posicion en y.
+    private ImageIcon icono;    //icono.
+    Animacion animacion;
+
+    /**
+     * Metodo constructor usado para crear el objeto
+     *
+     * @param posX es la <code>posicion en x</code> del objeto.
+     * @param posY es la <code>posicion en y</code> del objeto.
+     * @param animacion es la <code>animacion</code> del objeto.
+     */
+    public Base (int posX, int posY, Animacion animacion) {
+        this.posX = posX;
+        this.posY = posY;
+        this.animacion = animacion;
+    }
+
+    /**
+     * Metodo modificador usado para cambiar la posicion en x del objeto
+     *
+     * @param posX es la <code>posicion en x</code> del objeto.
+     */
+    public void setPosX (int posX) {
+        this.posX = posX;
+    }
+
+    /**
+     * Metodo de acceso que regresa la posicion en x del objeto
+     *
+     * @return posX es la <code>posicion en x</code> del objeto.
+     */
+    public int getPosX () {
+        return posX;
+    }
+
+    /**
+     * Metodo modificador usado para cambiar la posicion en y del objeto
+     *
+     * @param posY es la <code>posicion en y</code> del objeto.
+     */
+    public void setPosY (int posY) {
+        this.posY = posY;
+    }
+
+    /**
+     * Metodo de acceso que regresa la posicion en y del objeto
+     *
+     * @return posY es la <code>posicion en y</code> del objeto.
+     */
+    public int getPosY () {
+        return posY;
+    }
+
+    // Metodo que ajusta la imagen del objeto
+    public void setAnimacion (Animacion anim) {
+        this.animacion = anim;
+    }
+
+    // Metodo que regresa la imagen
+    public Animacion getAnimacion () {
+        return this.animacion;
+    }
+
+    /**
+     * Metodo de acceso que regresa el ancho del icono
+     *
+     * @return un objeto de la clase <code>ImageIcon</code> que es el ancho del
+     * icono.
+     */
+    public int getAncho () {
+        return new ImageIcon(this.animacion.getImagen()).getIconWidth();
+    }
+
+    /**
+     * Metodo de acceso que regresa el alto del icono
+     *
+     * @return un objeto de la clase <code>ImageIcon</code> que es el alto del
+     * icono.
+     */
+    public int getAlto () {
+        return new ImageIcon(this.animacion.getImagen()).getIconHeight();
+    }
+
+    /**
+     * Metodo de acceso que regresa un nuevo rectangulo
+     *
+     * @return un objeto de la clase <code>Rectangle</code> que es el perimetro
+     * del rectangulo
+     */
+    public Rectangle getPerimetro () {
+        return new Rectangle(getPosX(), getPosY(), getAncho(), getAlto());
+    }
+
+    /**
+     * Checa si el objeto <code>Movement</code> intersecta a otro
+     * <code>Movement</code>
+     *
+     * @return un valor boleano <code>true</code> si lo intersecta
+     * <code>false</code> en caso contrario
+     */
+    public boolean intersecta (Base obj) {
+        return getPerimetro().intersects(obj.getPerimetro());
+    }
+    
+}
--- STTD/src/sttd/STTD.java
+++ STTD/src/sttd/STTD.java
@@ -2,11 +2,72 @@ package sttd;
 
 /**
  * Star Wars Tower Defense
-021 * Developed by Mad Devs
+ * Developed by Mad Devs
  * "Stay on the rigth side, the Made side"
  * Version: Alpha       Date: 4/07/2014
  */
-public class STTD {
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.LinkedList;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class STTD extends JFrame implements Runnable, KeyListener, MouseListener {
+
+    public STTD(){
+        
+    }
+    
+    public void run() {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
 
     /**
      * @param args the command line arguments
@@ -14,5 +75,4 @@ public class STTD {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
 }
