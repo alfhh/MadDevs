32cc6abfdbdb7fe13800816e56fe6f73f0275b9a
Luis Juan Sanchez <luisjuansp@gmail.com>
2014-04-07 14:26:35 -0500

    Animacion and SoundClip classes

--- STTD/src/sttd/STTD.java
+++ STTD/src/sttd/STTD.java
@@ -3,14 +3,69 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sttd;
 
-/**
- *
- * @author LuisJuan
- */
-public class STTD {
+import java.awt.Color;
+import javax.swing.JFrame;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Font;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.LinkedList;
+import java.awt.Graphics;
+import javax.swing.JOptionPane;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class STTD extends JFrame implements Runnable, KeyListener, MouseListener {
+
+    public STTD(){
+        
+    }
+    
+    public void run() {
+
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+
+    }
+
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    public void mouseExited(MouseEvent e) {
+
+    }
 
     /**
      * @param args the command line arguments
@@ -18,5 +73,5 @@ public class STTD {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
