6424c79ac8e4814bdcb6c76c44f26f87ee01a7a9
Luis Juan Sanchez <luisjuansp@gmail.com>
2014-04-25 21:12:55 -0500

    Start and End sets and gets

--- STTD/src/sttd/Enemy.java
+++ STTD/src/sttd/Enemy.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package sttd;
 
 import java.awt.Point;
@@ -13,6 +12,7 @@ import java.awt.Point;
  * @author LuisJuan
  */
 public class Enemy extends Base {
+
     int type = 0; //tipo de enemigo.
     Point start;
     Point end;
@@ -26,36 +26,61 @@ public class Enemy extends Base {
     int towerid; //Id de la enemigo (Utilizado para identificar la enemigo independientemente de su lugar en la linked list)
     int value; //Valor de venta de la enemigo
     int target = 0; // enemigo a apuntar
-    
-    
-    
-    public Enemy (int posX, int posY, Animacion animacion, int t) {
+
+    public Enemy(int posX, int posY, Animacion animacion, int t) {
         super(posX, posY, animacion);
         type = t;
-        end = new Point(posX,posY);
-        start = new Point(posX,posY);
+        end = new Point(posX, posY);
+        start = new Point(posX, posY);
     }
+
     //Metodo para obtener el angulo de la enemigo
-    double getAngle()
-    {
+    double getAngle() {
         return angle;
     }
-    
+
     //Metodo para asignar el angulo de la enemigo
-    void setAngle(double a)
-    {
+    void setAngle(double a) {
         angle = a;
     }
-    
+
     //Metodo para obtener el angulo de la enemigo
-    int getSpeed()
-    {
+    int getSpeed() {
         return speed;
     }
-    
+
     //Metodo para asignar el angulo de la enemigo
-    void setSpeed(int s)
-    {
+    void setSpeed(int s) {
         speed = s;
+    }
+
+    //Metodo para asignar el movimiento
+    void setMov(char m) {
+        movment = m;
+    }
+
+    //Metodo para obtener el movimiento
+    char getMov() {
+        return movment;
+    }
+
+    //Metodo para asignar el bloque anterior
+    void setStart(Point p) {
+        start.setLocation(p.getLocation());
+    }
+
+    //Metodo para obtener el bloque anterior
+    Point getStart() {
+        return start;
+    }
+
+    //Metodo para asignar el bloque siguiente
+    void setEnd(Point p) {
+        end.setLocation(p.getLocation());
+    }
+
+    //Metodo para obtener el movimiento
+    Point getEnd() {
+        return end;
     }
 }
